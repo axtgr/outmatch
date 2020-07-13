8876b4976ef6e92a7bcc61fc49a311dd7a1c1008
Alex Schneider <me@schneider.ax>
2020-07-13 21:53:34 +0700

    Update benchmark

--- bench/bench.js
+++ bench/bench.js
@@ -1,4 +1,4 @@
-var { Suite } = require('benchmark')
+var Suite = require('benchmark').Suite
 var globrex = require('globrex')
 var picomatch = require('picomatch')
 var matcher = require('matcher')
@@ -31,7 +31,7 @@ var OPTIONS = {
     noglobstar: false,
   },
 }
-var REGEXPS = {
+var MATCHERS = {
   outmatch: outmatch('src/**/*.?s', OPTIONS.outmatch),
   outmatchSep: outmatch('src/**/*.?s', OPTIONS.outmatchSep),
   globrex: globrex('src/**/*.?s', OPTIONS.globrex).regex,
@@ -40,60 +40,79 @@ var REGEXPS = {
 }
 
 function pattern() {
+  // Making sure the engine doesn't optimize for static strings
   var str = 'src'
-  return `${str || 'asd'}/zxc/**/*.?${'s'}`
+  return (str || 'asd') + '/zxc/**/*.?s'
 }
 
 function sample() {
   return 'src/test/foo.js'
 }
 
+function compile(fn, options) {
+  return function () {
+    fn(pattern(), options)
+  }
+}
+
+function match(matcher) {
+  if (matcher instanceof RegExp) {
+    return function () {
+      return matcher.test(sample())
+    }
+  } else {
+    return function () {
+      return matcher(sample())
+    }
+  }
+}
+
 new Suite('Compilation')
-  .add('outmatch', () => outmatch(pattern()), OPTIONS.outmatch)
-  .add('outmatch separated', () => outmatch(pattern(), OPTIONS.outmatchSep))
-  .add('globrex', () => globrex(pattern(), OPTIONS.globrex))
-  .add('globrex separated', () => globrex(pattern(), OPTIONS.globrexSep))
-  .add('picomatch', () => picomatch(pattern()))
-  .add('picomatch separated', () => picomatch(pattern(), OPTIONS.picomatchSep))
-  .on('start', (event) => {
+  .add('outmatch', compile(outmatch, OPTIONS.outmatch))
+  .add('outmatch separated', compile(outmatch, OPTIONS.outmatchSep))
+  .add('globrex', compile(globrex, OPTIONS.globrex))
+  .add('globrex separated', compile(globrex, OPTIONS.globrexSep))
+  .add('picomatch', compile(picomatch))
+  .add('picomatch separated', compile(picomatch, OPTIONS.picomatchSep))
+  .on('start', function (event) {
     console.log(event.currentTarget.name)
   })
-  .on('cycle', (event) => {
-    var name = event.target.name.padEnd('outmatch with no separator'.length + 2)
+  .on('cycle', function (event) {
+    var name = event.target.name.padEnd('picomatch separated'.length + 2)
     var hz = formatNumber(event.target.hz.toFixed(0)).padStart('100,000,000'.length + 2)
     console.log(' ', name, hz, 'ops/sec')
   })
   .run()
 
 new Suite('Matching')
-  .add('outmatch', () => REGEXPS.outmatch.test(sample()))
-  .add('outmatch separated', () => REGEXPS.outmatchSep.test(sample()))
-  .add('globrex', () => REGEXPS.globrex.test(sample()))
-  .add('globrex separated', () => REGEXPS.globrexSep.test(sample()))
-  .add('picomatch separated', () => REGEXPS.picomatchSep(sample()))
-  .add('matcher', () => matcher.isMatch(pattern(), sample()))
-  .on('start', (event) => {
-    console.log(`\n${event.currentTarget.name}`)
+  .add('outmatch', match(MATCHERS.outmatch))
+  .add('outmatch separated', match(MATCHERS.outmatchSep))
+  .add('globrex', match(MATCHERS.globrex))
+  .add('globrex separated', match(MATCHERS.globrexSep))
+  .add('picomatch separated', match(MATCHERS.picomatchSep))
+  .add('matcher', match(matcher.isMatch.bind(null, pattern())))
+  .on('start', function (event) {
+    console.log('\n' + event.currentTarget.name)
   })
-  .on('cycle', (event) => {
-    var name = event.target.name.padEnd('anymatch - precompiled'.length + 2)
+  .on('cycle', function (event) {
+    var name = event.target.name.padEnd('picomatch separated'.length + 2)
     var hz = formatNumber(event.target.hz.toFixed(0)).padStart('100,000,000'.length + 2)
     console.log(' ', name, hz, 'ops/sec')
   })
   .run()
 
 // Compilation
-//   outmatch                     1,772,519 ops/sec
-//   outmatch separated           1,429,059 ops/sec
-//   globrex                      1,185,760 ops/sec
-//   globrex separated              377,129 ops/sec
-//   picomatch                      190,596 ops/sec
-//   picomatch separated            190,609 ops/sec
+//   outmatch                  1,803,039 ops/sec
+//   outmatch separated        1,483,489 ops/sec
+//   globrex                   1,249,040 ops/sec
+//   globrex separated           379,373 ops/sec
+//   picomatch                   193,992 ops/sec
+//   picomatch separated         191,981 ops/sec
 
 // Matching
-//   outmatch                    28,155,155 ops/sec
-//   outmatch separated          25,603,147 ops/sec
-//   globrex                     26,169,739 ops/sec
-//   globrex separated           22,374,638 ops/sec
-//   picomatch separated         10,524,039 ops/sec
-//   matcher                      1,200,544 ops/sec
+//   outmatch                 26,669,481 ops/sec
+//   outmatch separated       25,857,271 ops/sec
+//   globrex                  26,247,172 ops/sec
+//   globrex separated        23,403,820 ops/sec
+//   picomatch separated      10,574,971 ops/sec
+//   matcher                   1,618,739 ops/sec
